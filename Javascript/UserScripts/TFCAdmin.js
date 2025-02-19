058d4199339444bd8fd762619d62b9035c391042
johnnyedev <156373071+johnnyedev@users.noreply.github.com>
2025-02-18 18:21:47 -0600

    Update and rename TFCAdmin.js to TFCAdmin.js

--- Javascript/UserScripts/TFCAdmin.js
+++ Javascript/UserScripts/TFCAdmin.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.0.2
 // @description  try to take over the world!
-// @author       https://github.com/johnnyedev/
+// @author       https://github.com/johnnyedev
 // @match        https://app.terraform.io/app/admin/organizations/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=terraform.io
 // @grant        GM_addStyle
